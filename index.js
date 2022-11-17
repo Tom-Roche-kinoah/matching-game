ac0b30d15310d67b21b3bd6b440ded7c830b4194
Tom R <99020119+Tom-Roche-kinoah@users.noreply.github.com>
2022-11-17 15:25:06 +0100

    feat: app entry point

---
+++ index.js
@@ -0,0 +1,5 @@
+require('./server');
+// le point d'entrée de notre application
+// il va charger l'index.js qui se trouve dans le dossier /server
+
+// Pour lancer l'application en mode dev : npm run dev
